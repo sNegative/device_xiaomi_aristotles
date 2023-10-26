4c8392936a6c3aec09d55d089ace49ba5c9c6b2e
Jorge <jorgeim97@gmail.com>
2023-10-26 13:01:00 +0200

    Initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_aristotle.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_aristotle-user \
+    lineage_aristotle-userdebug \
+    lineage_aristotle-eng
---
+++ README.md
@@ -0,0 +1,9 @@
+# Android device tree for Xiaomi 2306EPN60G (aristotle)
+
+```
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
